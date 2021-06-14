dc90e83619597ecf2e3c6f13774696efde18b124
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-06-15 00:55:09 +0200

    BankTransactionsProducer adding properties

---
+++ src/main/java/org/example/kafka/stream/BankTransactionsProducer.java
@@ -0,0 +1,28 @@
+package org.example.kafka.stream;
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class BankTransactionsProducer {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all"); // strongest producing guarantee
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, "3");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "1");
+        // leverage idempotent producer from Kafka 0.11 !
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+    }
+
+}
