4f3683ac87f8053cc21677f3e99c3cb51127d8ff
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-06-15 18:40:14 +0200

    json Serde

--- src/main/java/org/example/kafka/stream/BankBalanceExactlyOnce.java
+++ src/main/java/org/example/kafka/stream/BankBalanceExactlyOnce.java
@@ -1,7 +1,23 @@
 package org.example.kafka.stream;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.KTable;
+
+import java.time.Instant;
+import java.util.Properties;
 
 import java.util.Properties;
 
@@ -17,5 +33,10 @@ public class BankBalanceExactlyOnce {
 
         // Exactly once processing
         config.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
+
+        final Serializer<JsonNode> jsonSerializer = new JsonSerializer();
+        final Deserializer<JsonNode> jsonDeserializer = new JsonDeserializer();
+        final Serde<JsonNode> jsonSerde = Serdes.serdeFrom(jsonSerializer, jsonDeserializer);
+
     }
 }
