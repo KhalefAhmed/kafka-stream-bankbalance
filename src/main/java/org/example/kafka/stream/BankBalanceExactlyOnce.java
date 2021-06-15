6fc61649d718d261f7f859634d5d10d7fc673f63
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-06-15 18:32:15 +0200

    configure my kafka stream

--- src/main/java/org/example/kafka/stream/BankBalanceExactlyOnce.java
+++ src/main/java/org/example/kafka/stream/BankBalanceExactlyOnce.java
@@ -1,4 +1,21 @@
 package org.example.kafka.stream;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.util.Properties;
+
 public class BankBalanceExactlyOnce {
+    public static void main(String[] args) {
+        Properties config = new Properties();
+
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "bank-balance-application");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        config.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
+
+        // Exactly once processing
+        config.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
+    }
 }
